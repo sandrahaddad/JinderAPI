df931747d9fce53debfef167d57664a11cea22aa
sandrahaddad <45422489+sandrahaddad@users.noreply.github.com>
2018-12-17 16:26:13 +0200

    j

---
+++ JinderAPI/JinderAPI/Models/JinderSeeker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JinderAPI.Models
+{
+    public class JinderSeeker
+    {
+        public string username { get; set; }
+    }
+}
